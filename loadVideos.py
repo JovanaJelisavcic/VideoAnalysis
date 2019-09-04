8bbe69799e889665f997706be154cb929c41401a
jovana jelisavcic <jelisavcicjovana@gmail.com>
2019-09-04 22:13:45 +0200

    Hough Line Transformation used to detect the line, also tried to detect it over color

--- loadVideos.py
+++ loadVideos.py
@@ -6,6 +6,15 @@ width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 while True:
     ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+    for x1, y1, x2, y2 in lines[0]:
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
     if ret:
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(frame, 'ZBIR: ', (width-150, height-20), font, 1, (255, 255, 255), 5, cv2.LINE_AA)
@@ -16,3 +25,12 @@ while True:
         break
 video_capture.release()
 cv2.destroyAllWindows()
+""" 
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+    """
